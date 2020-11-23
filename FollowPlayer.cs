62fe99887a0a2d68ff39c52230f3595391ac21e9
DakshalP <dakshal29@gmail.com>
2020-11-23 11:17:22 -0500

    add movement and follow scripts

---
+++ FollowPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // the position of the player
+    public Transform playerTransform;
+    
+    //how much you want to offset the camera by in 3D space
+    public Vector3 offset = new Vector3(0f, 2f, -5f);
+
+    void Update() {
+        //if you want it to always follow
+        //transform.position = playerTransform.position + offset;
+
+        //follow, but stop at a certain point so you can see pins falling
+        if(transform.position.z < 15) transform.position = playerTransform.position + offset;
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    /*
+    test code: 
+    (just to learn, don't put this in the final game)
+
+    //runs once
+    void Start() {
+        Debug.Log("Hello World");
+    }
+
+    //runs every frame
+    void Update() {
+        if(Input.GetKey("a")) {
+            Debug.Log("You pressed A");
+        }
+    }
+
+    */
+
+   //game code
+   public Rigidbody rb;
+   public float thrust = 40.0f;
+   public float sidewaysThrust = 400.0f;
+
+   public bool spacePressed = false;
+
+   void Update () {
+       if(Input.GetKeyDown("space")) {
+           spacePressed = true;
+       }
+   }
+
+   //also called once per frame, but adjusts for different framerates
+   void FixedUpdate() {
+       if(spacePressed) {
+           rb.AddForce(0, 0, thrust * Time.deltaTime, ForceMode.Impulse);
+       } else {
+           if(Input.GetKey("d")) {
+                rb.AddForce(sidewaysThrust * Time.deltaTime, 0, 0);
+            }
+            else if(Input.GetKey("a")) {
+                rb.AddForce(-sidewaysThrust * Time.deltaTime, 0, 0);
+            }
+       }
+       
+   }
+}
