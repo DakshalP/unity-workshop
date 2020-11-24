eb835f6d604b3f0bbd4d92b42d24709cb2630832
DakshalP <dakshal29@gmail.com>
2020-11-23 19:05:47 -0500

    add pin tipping detection

---
+++ PinController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PinController : MonoBehaviour
+{
+    Transform t;
+
+    bool pinChecked = false;
+
+    void Start() {
+        t = GetComponent<Transform>();
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        //if it hits anything other than the ground
+        if(collision.gameObject.name != "Ground") {
+            //PinHit()
+
+            //wait 2 seconds to allow it to tip
+            Invoke("PinHit", 2);
+        }
+    }
+
+    void PinHit() {
+        if(pinChecked) {
+            return;
+        }
+
+        pinChecked = true;
+
+        //Debug.Log("Pin Hit")
+
+        //check if tipped
+        float currentRotation = t.rotation.eulerAngles.z;
+        if( !(currentRotation < 10 || currentRotation > 350)) {
+
+            Debug.Log("Tipped over");
+
+        } else {
+
+            Debug.Log("Not tipped over");
+
+        }
+    }
+}
